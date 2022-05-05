5bd42a1e861581a2316b5a07eb1107891a0c8aaf
ShepDogFGC <71411118+ShepDogFGC@users.noreply.github.com>
2022-05-05 16:35:30 -0400

    Makin money moves !!

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+  return(
+    <Layout pageTitle="About Me">
+      <p>Hello! My name is Nicholas Shepard. I am a Software Engineer currently based in Columbus, Ohio. I'm finishing my Bachalors of Science in Computer Science & Engineering from The Ohio State University, I'm also achieving a minor in Philosophy as well.</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
